ff29e1cd53f8184aaa997b469d83a88f032d65d8
Moatasim E. V <46879599+MoatasimV@users.noreply.github.com>
2023-10-20 16:53:20 +0300

    Update card-ways.com.js

--- card-ways.com.js
+++ card-ways.com.js
@@ -1,5 +1,3 @@
-$(document).ready(function(){
-   setTimeout(function(){
-      PopUp();
-   },300); // 5000 to load it after 5 seconds from page load
-});
+window.onload = function() {
+  alert("This is an alert message!");
+};
